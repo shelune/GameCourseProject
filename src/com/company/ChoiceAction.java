8b8da26de1b5d1bf992aa3aa4f03899aa0724f80
akzuki <iamakzuki@gmail.com>
2015-02-05 23:17:53 +0200

    Update ChoiceAction.java

--- src/com/company/ChoiceAction.java
+++ src/com/company/ChoiceAction.java
@@ -1,11 +1,10 @@
 package com.company;
-
 /**
  * Created by ASUS on 05-Feb-15.
  */
 public class ChoiceAction {
     private String sayString;
-    private int[] statsChanged = new int[] {0, 0, 0, 0};
+    private int[] statsChanged = new int[] {0, 0, 0, 0}; // COU - UND - ABN - STAMINA
 
     public String getSayString() {
         return sayString;
@@ -44,10 +43,49 @@ public class ChoiceAction {
             case 1:
                 statsChanged[2] = -1;
                 statsChanged[3] = 30;
+                break;
         }
         return statsChanged;
     }
-
+    
+    public int[] statsEvent1(int choice) {
+    	switch (choice)	{
+    		case 0:
+    			statsChanged[0] = -2;
+    			statsChanged[2] = 2;
+    			break;
+    		case 1:
+    			statsChanged[0] = 1;
+    			statsChanged[2] = 1;
+    			break;
+    		case 2:
+    			statsChanged[0] = 2;
+    			statsChanged[1] = -1;
+    			break;
+    		case 3:
+    			statsChanged[0] = -3;
+    			break;
+    	}
+    	return statsChanged;
+    }
+    
+    public int[] statsEvent2(int choice) {
+    	switch (choice)	{
+    		case 0:
+    			statsChanged[1] = 1;
+    			break;
+    		case 1:
+    			break;
+    		case 2:
+    			break;
+    		case 3:
+    			statsChanged[0] = 1;
+    			statsChanged[1] = -3;
+    			break;
+    	}
+    	return statsChanged;
+    }
+    
     public ChoiceAction(String sayString) {
         super();
         this.sayString = sayString;

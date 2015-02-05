ffcdcd2d7e33cb2739b6e341ff6dce9198400985
akzuki <iamakzuki@gmail.com>
2015-02-05 23:15:11 +0200

    Update Events.java

--- src/com/company/Events.java
+++ src/com/company/Events.java
@@ -11,42 +11,89 @@ public class Events {
 
     public int[] getEventFirstNight(ArrayList<String> eventTrigger) {
         int[] statsChanged = new int[] {0, 0, 0, 0};                    // COU - UND - ABN - STAMINA
-        if (eventTrigger.contains("0A")) {
+        if (isTriggered("0A")) {
             return statsChanged;
         }
-        int i;
         String[] event = eventDial.getFirstNight();
-        for (i = 0; i < 7; i++) {
-            say(event[i]);
-            next();
+        for (String s : event) {
+            next(); say(s);
         }
-        int choice = -1;
-        while (choice > 2 || choice < 0) {
-            try {
-                say(event[7]);
-                choice = input.nextInt();
-            } catch (InputMismatchException e) {
-                next();
-            }
-            switch (choice) {
-                case 0:
-                    say(event[8]); say(event[9]);
-                    statsChanged[0] = 2;
-                    break;
-                case 1:
-                    say(event[10]); say (event[11]);
-                    statsChanged[1] = 2;
-                    break;
-                case 2:
-                    say(event[12]);
-                    break;
-            }
+        int choice = takeInput(2);
+        for (ChoiceAction ca : eventDial.getActionFirstNight().get(choice)) {
+            say(ca.getSayString()); next();
+            statsChanged = ca.statsFirstNight(choice);
         }
-        say(event[13]);
         eventTrigger.add("0A");
+        say(eventDial.getFirstNightEnd());
         return statsChanged;
     }
 
+    public int[] getEventFirstSeeNumbers(ArrayList<String> eventTrigger) {
+        int[] statsChanged = new int[] {0, 0, 0, 0};                    // COU - UND - ABN - STAMINA
+        if (eventTrigger.contains("1A")) {
+            return statsChanged;
+        }
+        int i;
+        String[] event = eventDial.getFirstSeeNumbers_p1();
+        for (String s : event) {
+            next(); say(s);
+        }
+        int choice = takeInput(1);
+        for (ChoiceAction ca : eventDial.getActionFirstSeeNum_p1().get(choice)) {
+            say(ca.getSayString());
+            next();
+            statsChanged = ca.statsFirstSeeNum_p1(choice);
+        }
+        event = eventDial.getFirstSeeNumbers_p2();
+        for (String s : event) {
+            next(); say(s);
+        }
+        choice = takeInput(1);
+        for (ChoiceAction ca : eventDial.getActionFirstSeeNum_p2().get(choice)) {
+            say(ca.getSayString());
+            next();
+            statsChanged = ca.statsFirstSeeNum_p2(choice);
+        }
+        eventTrigger.add("1A");
+        return statsChanged;
+    }
+    
+    public int[] getEventREvent1(ArrayList<String> eventTrigger) {
+        int[] statsChanged = new int[] {0, 0, 0, 0};                    // COU - UND - ABN - STAMINA
+        if (isTriggered("R1")) {
+            return statsChanged;
+        }
+        String[] event = eventDial.getREvent1();
+        for (String s : event) {
+            next(); say(s);
+        }
+        int choice = takeInput(3);
+        for (ChoiceAction ca : eventDial.getActionREvent1().get(choice)) {
+            say(ca.getSayString()); next();
+            statsChanged = ca.statsEvent1(choice);
+        }
+        eventTrigger.add("R1");
+        return statsChanged;
+    }
+    
+    public int[] getEventREvent2(ArrayList<String> eventTrigger) {
+        int[] statsChanged = new int[] {0, 0, 0, 0};                    // COU - UND - ABN - STAMINA
+        if (isTriggered("R2")) {
+            return statsChanged;
+        }
+        String[] event = eventDial.getREvent2();
+        for (String s : event) {
+            next(); say(s);
+        }
+        int choice = takeInput(3);
+        for (ChoiceAction ca : eventDial.getActionREvent2().get(choice)) {
+            say(ca.getSayString()); next();
+            statsChanged = ca.statsEvent2(choice);
+        }
+        eventTrigger.add("R2");
+        return statsChanged;
+    }
+    
     public void next() {
         input.nextLine();
     }
@@ -73,5 +120,17 @@ public class Events {
 
     public static void say(String text) {
         System.out.println(text);
+    }
+
+    public int takeInput(int upperLim) {
+        int choice = -1;
+        while (choice > upperLim || choice < 0) {
+            try {
+                choice = input.nextInt();
+            } catch (InputMismatchException e) {
+                next();
+            }
+        }
+        return choice;
     }
 }

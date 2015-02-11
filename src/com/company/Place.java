79d88504fc7a164d1195adaf0886b4996a69e78e
akzuki <iamakzuki@gmail.com>
2015-02-11 23:14:15 +0200

    Update Place.java

--- src/com/company/Place.java
+++ src/com/company/Place.java
@@ -1,21 +1,20 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Collections;
 
 /**
  * Created by ASUS on 29-Jan-15.
  */
 public class Place {
-    private Random rand = new Random();
     private Dialogue dialogue = new Dialogue();
 
     private int placeNumber;
     private String placeName;
     private String placeDescription;
-    private ArrayList<Item> itemList = new ArrayList<Item>();
     private String accessCode;
     private ArrayList<String> accessTo = new ArrayList<String>();
+    private ArrayList<Item> itemList = new ArrayList<Item>();
 
     public Place(int num, String name, String description, String access) {
         this.placeNumber = num;
@@ -45,21 +44,26 @@ public class Place {
         return this.placeName;
     }
 
-    public void loadHomeItems() {
-        Item apple = new Item("Apple", "Recovers 10 Stamina", 10, 1);
-        Item phone = new Item("Phone", "Used to call someone");
-        Item magnifier = new Item("Magnifier", "Used to see tiny things");
-        itemList.add(apple);
-        itemList.add(phone);
-    }
+    public void setItems() {
+        ArrayList<Item> temp = new ArrayList<Item>();
+        switch (placeNumber) {
+            case 0:
+                // Item apple = new Item("Apple", "Recovers 10 Stamina", 10, 1);
+                Item phone = new Item("Phone", "Used to call someone");
+                Item magnifier = new Item("Magnifier", "Used to see tiny things");
+                Collections.addAll(temp, phone, magnifier);
+                break;
+            case 1:
+                Item calculator = new Item("Calculator", "A mechanical calculator for complex problems");
+                Collections.addAll(temp, calculator);
+                break;
+            case 4:
+                Item statue = new Item("Status", "A small statue with a small crack on it", "KEY INSIDE");
+                Collections.addAll(temp, statue);
+                break;
 
-    public void loadJanitorItems() {
-        Item note = new Item("Small Note", dialogue.getJanitorNote());
-        itemList.add(note);
-    }
-
-    public void removeItem(Item item) {
-        itemList.remove(item);
+        }
+        itemList = temp;
     }
 
     @Override

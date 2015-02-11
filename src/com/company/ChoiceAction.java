140998d15b8fca704bea237afea4bf5e2347b009
akzuki <iamakzuki@gmail.com>
2015-02-11 23:11:24 +0200

    Create ChoiceAction.java

---
+++ src/com/company/ChoiceAction.java
@@ -0,0 +1,196 @@
+Skip to content
+ This repository
+Explore
+Gist
+Blog
+Help
+akzuki akzuki
+ 
+1  Unwatch 
+  Star 0
+ Fork 0akzuki/GameProject
+ branch: master  GameProject/src/gameproject/ChoiceAction.java
+akzukiakzuki 7 hours ago con cu
+1 contributor
+180 lines (166 sloc)  4.256 kb RawBlameHistory   
+package com.company;
+/**
+ * Created by ASUS on 05-Feb-15.
+ */
+public class ChoiceAction {
+    private String sayString;
+    private int[] statsChanged = new int[] {0, 0, 0, 0}; // COU - UND - ABN - STAMINA
+
+    public String getSayString() {
+        return sayString;
+    }
+
+    public int[] statsFirstNight(int choice) {
+        switch (choice) {
+            case 0:
+                statsChanged[0] = 2;
+                break;
+            case 1:
+                statsChanged[1] = 2;
+                break;
+            case 2:
+                break;
+        }
+        return statsChanged;
+    }
+    
+    public int[] statsFirstDeath(int choice)	{
+    	switch (choice) {
+        case 0:
+            statsChanged[0] = 2;
+            statsChanged[1] = 1;
+            break;
+        case 1:
+            statsChanged[0] = -1;
+            statsChanged[1] = -1;
+            break;
+    	}
+    	return statsChanged;
+    }
+    	
+    public int[] statsFirstSeeNum_p1(int choice) {
+        switch (choice) {
+            case 0:
+                statsChanged[2] = 2;
+                break;
+            case 1:
+                break;
+        }
+        return statsChanged;
+    }
+
+    public int[] statsFirstSeeNum_p2(int choice) {
+        switch (choice) {
+            case 0:
+                statsChanged[2] = 1;
+                break;
+            case 1:
+                statsChanged[2] = -1;
+                statsChanged[3] = 30;
+                break;
+        }
+        return statsChanged;
+    }
+    
+    public int[] statsEvent1(int choice) {
+    	switch (choice)	{
+    		case 0:
+    			statsChanged[0] = -2;
+    			statsChanged[2] = 2;
+    			break;
+    		case 1:
+    			statsChanged[0] = 1;
+    			statsChanged[2] = 1;
+    			break;
+    		case 2:
+    			statsChanged[0] = 2;
+    			statsChanged[1] = -1;
+    			break;
+    		case 3:
+    			statsChanged[0] = -3;
+    			break;
+    	}
+    	return statsChanged;
+    }
+    
+    public int[] statsEvent2(int choice) {
+    	switch (choice)	{
+    		case 0:
+    			statsChanged[1] = 1;
+    			break;
+    		case 1:
+    			break;
+    		case 2:
+    			break;
+    		case 3:
+    			statsChanged[0] = 1;
+    			statsChanged[1] = -3;
+    			break;
+    	}
+    	return statsChanged;
+    }
+
+    public int[] statsEvent3(int choice, Inventory inventory) {
+        switch (choice)	{
+            case 0:
+                statsChanged[1] = -4;
+                break;
+            case 1:
+                statsChanged[0] = 2;
+                inventory.addItem(new Item("Bike", "Make it easier for you to go around"));
+                break;
+            case 2:
+                statsChanged[1] = -4;
+                break;
+            case 3:
+                statsChanged[4] = 50;
+                break;
+        }
+        return statsChanged;
+    }
+
+    
+    public int[] statsSeeJanitorNumber(int choice, Inventory inventory) {
+        switch (choice)	{
+            case 0:
+                statsChanged[0] = 1;
+                statsChanged[2] = 2;
+                break;
+            case 1:
+                inventory.addItem(new Item("Cake", "Recovers 20 Stamina", 10, 1));
+                break;
+            case 2:
+                break;
+            }
+        return statsChanged;
+    }
+    
+    public int[] statsGetBullied(int choice, int und) {
+        switch (choice)	{
+            case 0:
+                statsChanged[0] = -1;
+                statsChanged[2] = 1;
+                break;
+            case 1:
+                if (und > 10) {
+                    statsChanged[0] = 2;
+                } else {
+                    statsChanged[3] = -100;
+                }
+                break;
+            case 2:
+                statsChanged[0] = 1;
+                break;
+            case 3:
+                statsChanged[3] = -101;
+                break;
+        }
+        return statsChanged;
+    }
+
+    public int[] statsMetJanitor(int choice) {
+        switch (choice) {
+            case 0:
+                break;
+            case 1:
+                statsChanged[0] = 1;
+                break;
+            case 2:
+                statsChanged[1] = -1;
+                break;
+        }
+        return statsChanged;
+    }
+    
+    public ChoiceAction(String sayString) {
+        super();
+        this.sayString = sayString;
+    }
+}
+Status API Training Shop Blog About
+© 2015 GitHub, Inc. Terms Privacy Security Contact
